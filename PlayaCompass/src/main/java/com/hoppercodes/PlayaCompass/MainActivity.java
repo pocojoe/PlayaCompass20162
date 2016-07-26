693914511a29f6dc3d2d48e24ac42e09c59f458c
jmiller <hoppercodes@gmail.com>
2016-07-25 23:45:21 -0700

    big move of sensors etc into PlayaPositioningSystem with string interface for display

--- PlayaCompass/src/main/java/com/hoppercodes/PlayaCompass/MainActivity.java
+++ PlayaCompass/src/main/java/com/hoppercodes/PlayaCompass/MainActivity.java
@@ -9,214 +9,63 @@ package com.hoppercodes.PlayaCompass;
 // 7/24/2016
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.hoppercodes.PlayaPositioningSystem.GoldenSpike;
-import com.hoppercodes.PlayaPositioningSystem.PlayaHeading;
-import com.hoppercodes.PlayaPositioningSystem.PlayaNavigate;
-import com.hoppercodes.PlayaPositioningSystem.PlayaPoint;
-import com.hoppercodes.PlayaPositioningSystem.PlayaPositioningSystem;
+import com.hoppercodes.PlayaCompass.R;
 
-public class MainActivity extends Activity implements SensorEventListener {
-    private LocationManager ppsLocationManager;
-    private SensorManager ppsSensorManager;
-    private Sensor ppsAccelerometer;
-    private Sensor ppsMagnetometer;
-    private String ppsProvider;
-    private PPSLocationListener ppsListener;
-    private PlayaPositioningSystem playaPositioningSystem;
+import com.hoppercodes.PlayaPositioningSystem.*;
 
-    // these need to be persistent, when I moved them within onSensorChanged things quit working
-    float[] lastAccelerometer = new float[3];
-    float[] lastMagnetometer = new float[3];
-    boolean lastAccelerometerSet = false;
-    boolean lastMagnetometerSet = false;
-    float[] mR = new float[9];
-    float[] mOrientation = new float[3];
+public class MainActivity extends Activity {
+    public PlayaPositioningSystem pps;
 
-
-    /**
-     * Called when the activity is first created.
-     */
-
-    // http://www.techrepublic.com/article/pro-tip-create-your-own-magnetic-compass-using-androids-internal-sensors/
-    // http://www.ymc.ch/en/smooth-true-north-compass-values  nice discussion of smoothing.
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        playaPositioningSystem = PlayaPositioningSystem.getInstance();
-        ppsLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        ppsSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        ppsAccelerometer = ppsSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        ppsMagnetometer = ppsSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        Criteria criteria;
-
-        criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);
-        criteria.setCostAllowed(false);
-        ppsProvider = ppsLocationManager.getBestProvider(criteria, false);
-        Location location = ppsLocationManager.getLastKnownLocation(ppsProvider);
-        ppsListener = new PPSLocationListener();
-        if (location != null) {
-            ppsListener.onLocationChanged(location);
-        } else {
-            // leads to the settings because there is no last known location
-            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(intent);
-            // TO DO
-            // need something here saying be patient, waiting for GPS
-        }
-        ppsLocationManager.requestLocationUpdates(ppsProvider, 200, 1, ppsListener);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        ppsLocationManager.requestLocationUpdates(ppsProvider, 200, 1, ppsListener);
-        ppsSensorManager.registerListener(this, ppsAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        ppsSensorManager.registerListener(this, ppsMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        ppsLocationManager.removeUpdates(ppsListener);
-        ppsSensorManager.unregisterListener(this, ppsAccelerometer);
-        ppsSensorManager.unregisterListener(this, ppsMagnetometer);
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        /*
-        these did not work to be declared here
-        float[] lastAccelerometer = new float[3];
-        float[] lastMagnetometer = new float[3];
-        boolean lastAccelerometerSet = false;
-        boolean lastMagnetometerSet = false;
-        float[] mR = new float[9];
-        float[] mOrientation = new float[3];
-        */
-        if (event.sensor == ppsAccelerometer) {
-            System.arraycopy(event.values, 0, lastAccelerometer, 0, event.values.length);
-            lastAccelerometerSet = true;
-        } else if (event.sensor == ppsMagnetometer) {
-            System.arraycopy(event.values, 0, lastMagnetometer, 0, event.values.length);
-            lastMagnetometerSet = true;
-        }
-        if (lastAccelerometerSet && lastMagnetometerSet) {
-            SensorManager.getRotationMatrix(mR, null, lastAccelerometer, lastMagnetometer);
-            SensorManager.getOrientation(mR, mOrientation);
-            float azimuthInRadians = mOrientation[0];
-            float azimuthInDegrees = (float) (Math.toDegrees(azimuthInRadians) + 360) % 360;
-            PlayaHeading updatedHere = new PlayaHeading(azimuthInDegrees);
-            playaPositioningSystem.setHeading(updatedHere);
-            PlayaDisplayBasics();
-        }
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // TODO Auto-generated method stub
-    }
-
-    private class PPSLocationListener implements LocationListener {
-        @Override
-        public void onLocationChanged(Location location) {
-            PlayaPoint updatedHere = new PlayaPoint(location.getLatitude(), location.getLongitude(), location.getAccuracy(), location.getProvider());
-            playaPositioningSystem.setHere(updatedHere);
-            Toast.makeText(MainActivity.this, "GPS Update",
-                    Toast.LENGTH_SHORT).show();
-            PlayaDisplayBasics();
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            Toast.makeText(MainActivity.this, provider + "'s status changed to " + status,
-                    Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-            Toast.makeText(MainActivity.this, "Provider " + provider + " enabled",
-                    Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-            Toast.makeText(MainActivity.this, "Provider " + provider + " disabled!",
-                    Toast.LENGTH_SHORT).show();
-        }
-    }
+    TextView gsname = (TextView) findViewById(R.id.gsName);
+    TextView gslatlon = (TextView) findViewById(R.id.gsLatLon);
+    TextView herename = (TextView) findViewById(R.id.hereName);
+    TextView herelatlon = (TextView) findViewById(R.id.hereLatLon);
+    TextView heremcadmdf = (TextView) findViewById(R.id.heremcadmdf);
+    TextView hereaddress = (TextView) findViewById(R.id.hereAddress);
+    TextView therename = (TextView) findViewById(R.id.thereName);
+    TextView therelatlon = (TextView) findViewById(R.id.thereLatLon);
+    TextView theremcadmdf = (TextView) findViewById(R.id.theremcadmdf);
+    TextView thereaddress = (TextView) findViewById(R.id.thereAddress);
+    TextView RoseInt = (TextView) findViewById(R.id.RoseDeg);
+    TextView RoseHeading = (TextView) findViewById(R.id.RoseHeading);
+    TextView RoseWind = (TextView) findViewById(R.id.RoseWind);
+    TextView navmagbearing = (TextView) findViewById(R.id.navMagBearing);
+    TextView navnorthbearing = (TextView) findViewById(R.id.navNorthBearing);
+    TextView navdistfeet = (TextView) findViewById(R.id.navDistFeet);
 
     void PlayaDisplayBasics() {
-        GoldenSpike gs = playaPositioningSystem.getGs();
-        PlayaHeading heading = playaPositioningSystem.getHeading();
-        PlayaPoint here = playaPositioningSystem.getHere();
-        PlayaPoint there = playaPositioningSystem.getThere();
-        PlayaNavigate hereToThere = new PlayaNavigate(); // TODO should this be in PPS? Circular
+        //PlayaNavigate hereToThere = new PlayaNavigate(); // TODO should this be in PPS? Circular
 
         setContentView(R.layout.activity_main);
         // golden spike
-        TextView gsname = (TextView) findViewById(R.id.gsName);
-        gsname.setText("*** Golden Spike: " + String.valueOf(gs.datum));
-        TextView gslatlon = (TextView) findViewById(R.id.gsLatLon);
-        gslatlon.setText(String.valueOf(gs.latlon));
+        gsname.setText(pps.gsdatum);
+        gslatlon.setText(pps.gslatlon);
 
         // here
-        TextView herename = (TextView) findViewById(R.id.hereName);
-        herename.setText("*** Here: " + String.valueOf(here.name));
-        TextView herelatlon = (TextView) findViewById(R.id.hereLatLon);
-        TextView heremcadmdf = (TextView) findViewById(R.id.heremcadmdf);
-        herelatlon.setText(String.valueOf(here.latlon));
-        heremcadmdf.setText(String.valueOf(here.mcadmdf));
-        TextView hereaddress = (TextView) findViewById(R.id.hereAddress);
-        hereaddress.setText(here.address);    // can increase precision if desired
+        herename.setText(pps.hereName);
+        herelatlon.setText(pps.hereLatLon);
+        heremcadmdf.setText(pps.hereMcadMdf);
+        hereaddress.setText(pps.hereAddress);    // can increase precision if desired
 
         // there
-        TextView therename = (TextView) findViewById(R.id.thereName);
-        therename.setText("*** There: " + String.valueOf(there.name));
-        TextView therelatlon = (TextView) findViewById(R.id.thereLatLon);
-        TextView theremcadmdf = (TextView) findViewById(R.id.theremcadmdf);
-        therelatlon.setText(String.valueOf(there.latlon));
-        theremcadmdf.setText(String.valueOf(there.mcadmdf));
-        TextView thereaddress = (TextView) findViewById(R.id.thereAddress);
-        thereaddress.setText(there.address);    // can increase precision if desired
+        therename.setText(pps.destName);
+        therelatlon.setText(pps.destLatLon);
+        theremcadmdf.setText(pps.destMcadMdf);
+        thereaddress.setText(pps.destAddress);    // can increase precision if desired
 
         // electronic compass  uses ppsAccelerometer and magnetic field sensors
-        TextView Rose = (TextView) findViewById(R.id.Rose);
-        Rose.setText("*** Compass Rose");
-        TextView RoseInt = (TextView) findViewById(R.id.RoseDeg);
-        RoseInt.setText(String.format("Mag: " + "%03d", heading.roseDeg));
-        TextView RoseHeading = (TextView) findViewById(R.id.RoseHeading);
-        RoseHeading.setText("Heading: " + String.valueOf(heading.roseHeading));
-        TextView RoseWind = (TextView) findViewById(R.id.RoseWind);
-        RoseWind.setText("Wind: " + String.valueOf(heading.roseWind));
+        RoseInt.setText(pps.roseDeg);
+        RoseHeading.setText(pps.roseHeading);
+        RoseWind.setText(pps.roseWind);
 
         // navigation route
-        TextView navigation = (TextView) findViewById(R.id.navigation);
-        navigation.setText("*** Navigation");
-        TextView navmagbearing = (TextView) findViewById(R.id.navMagBearing);
-        navmagbearing.setText("Mag:   " + String.format("%4.1f", hereToThere.navBearingM));
-        TextView navnorthbearing = (TextView) findViewById(R.id.navNorthBearing);
-        navnorthbearing.setText("North: " + String.format("%4.1f", hereToThere.navBearingN));
-        TextView navdistfeet = (TextView) findViewById(R.id.navDistFeet);
-        navdistfeet.setText("Nav Distance (FT): " + String.format("%6.0f", hereToThere.navdf));
+        navmagbearing.setText(pps.navBearingM);
+        navnorthbearing.setText(pps.navBearingN);
+        navdistfeet.setText(pps.navdf);
     }
 
     void MarkButtonClicked(View v) {
--- PlayaCompass/src/main/java/com/hoppercodes/PlayaPositioningSystem/PlayaPositioningSystem.java
+++ PlayaCompass/src/main/java/com/hoppercodes/PlayaPositioningSystem/PlayaPositioningSystem.java
@@ -1,24 +1,83 @@
 package com.hoppercodes.PlayaPositioningSystem;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.hoppercodes.PlayaCompass.R;
+
+
 /**
  * Created by hexagon on 6/24/16.
  */
-public final class PlayaPositioningSystem {
+public final class PlayaPositioningSystem extends Activity implements SensorEventListener {
     ///////////////////////////////////////////////////////////////
     GoldenSpike gs; // GoldenSpike(40.7864, -119.20065, 315.0, "GS 2016");
     PlayaHeading heading = new PlayaHeading(0f);
     PlayaPoint here = new PlayaPoint(40.7864, -119.2065, 10.0, "GS 2016");
     // PlayaPoint there = new PlayaPoint(40.7864, -119.2065, 10.0, "GS 2016");
     PlayaPoint there = new PlayaPoint(32.250018, -110.944092, 10.0, "Bookmans");
+    //GoldenSpike gs = playaPositioningSystem.getGs();
+    //PlayaHeading heading = playaPositioningSystem.getHeading();
+    //PlayaPoint here = playaPositioningSystem.getHere();
+    //PlayaPoint there = playaPositioningSystem.getThere();
+
+    public String gsdatum = "1";
+    public String gslatlon = "2";
+    public String hereName = "3";
+    public String hereLatLon = "4";
+    public String hereMcadMdf = "5";
+    public String hereAddress = "6";
+    public String destName = "7";
+    public String destLatLon = "8";
+    public String destMcadMdf = "9";
+    public String destAddress = "10";
+    public String roseDeg = "11";
+    public String roseHeading = "12";
+    public String roseWind = "13";
+    public String navBearingN = "14";
+    public String navBearingM = "15";
+    public String navdf = "16";
+
 
     private static PlayaPositioningSystem instance;
 
-    public static synchronized PlayaPositioningSystem getInstance(){
-        if(instance == null){
+    public static synchronized PlayaPositioningSystem getInstance() {
+        if (instance == null) {
             instance = new PlayaPositioningSystem();
         }
         return instance;
     }
+
+    private LocationManager ppsLocationManager;
+    private SensorManager ppsSensorManager;
+    private Sensor ppsAccelerometer;
+    private Sensor ppsMagnetometer;
+    private String ppsProvider;
+    private PPSLocationListener ppsListener;
+    private PlayaPositioningSystem playaPositioningSystem;
+
+    // these need to be persistent, when I moved them within onSensorChanged things quit working
+    float[] lastAccelerometer = new float[3];
+    float[] lastMagnetometer = new float[3];
+    boolean lastAccelerometerSet = false;
+    boolean lastMagnetometerSet = false;
+    float[] mR = new float[9];
+    float[] mOrientation = new float[3];
+
 
     private PlayaPositioningSystem() {
         this.gs = GoldenSpike.getInstance();
@@ -28,21 +87,19 @@ public final class PlayaPositioningSystem {
         return gs;
     }
 
-    public PlayaHeading getHeading()
-    {
+    public PlayaHeading getHeading() {
         return heading;
     }
 
     public void setHeading(PlayaHeading heading) {
-        this.heading=heading;
+        this.heading = heading;
     }
 
     public PlayaPoint getHere() {
         return here;
     }
 
-    public void setHere(PlayaPoint here)
-    {
+    public void setHere(PlayaPoint here) {
         this.here = here;
     }
 
@@ -52,5 +109,107 @@ public final class PlayaPositioningSystem {
 
     public void setThere(PlayaPoint there) {
         this.there = there;
+    }
+
+
+    // http://www.techrepublic.com/article/pro-tip-create-your-own-magnetic-compass-using-androids-internal-sensors/
+    // http://www.ymc.ch/en/smooth-true-north-compass-values  nice discussion of smoothing.
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        playaPositioningSystem = PlayaPositioningSystem.getInstance();
+        ppsLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        ppsSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        ppsAccelerometer = ppsSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        ppsMagnetometer = ppsSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        Criteria criteria;
+
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setCostAllowed(false);
+        ppsProvider = ppsLocationManager.getBestProvider(criteria, false);
+        Location location = ppsLocationManager.getLastKnownLocation(ppsProvider);
+        ppsListener = new PPSLocationListener();
+        if (location != null) {
+            ppsListener.onLocationChanged(location);
+        } else {
+            // leads to the settings because there is no last known location
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+            // TO DO
+            // need something here saying be patient, waiting for GPS
+        }
+        ppsLocationManager.requestLocationUpdates(ppsProvider, 200, 1, ppsListener);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ppsLocationManager.requestLocationUpdates(ppsProvider, 200, 1, ppsListener);
+        ppsSensorManager.registerListener(this, ppsAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        ppsSensorManager.registerListener(this, ppsMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        ppsLocationManager.removeUpdates(ppsListener);
+        ppsSensorManager.unregisterListener(this, ppsAccelerometer);
+        ppsSensorManager.unregisterListener(this, ppsMagnetometer);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor == ppsAccelerometer) {
+            System.arraycopy(event.values, 0, lastAccelerometer, 0, event.values.length);
+            lastAccelerometerSet = true;
+        } else if (event.sensor == ppsMagnetometer) {
+            System.arraycopy(event.values, 0, lastMagnetometer, 0, event.values.length);
+            lastMagnetometerSet = true;
+        }
+        if (lastAccelerometerSet && lastMagnetometerSet) {
+            SensorManager.getRotationMatrix(mR, null, lastAccelerometer, lastMagnetometer);
+            SensorManager.getOrientation(mR, mOrientation);
+            float azimuthInRadians = mOrientation[0];
+            float azimuthInDegrees = (float) (Math.toDegrees(azimuthInRadians) + 360) % 360;
+            PlayaHeading updatedHere = new PlayaHeading(azimuthInDegrees);
+            playaPositioningSystem.setHeading(updatedHere);
+            //PlayaDisplayBasics();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // TODO Auto-generated method stub
+    }
+
+    private class PPSLocationListener implements LocationListener {
+        @Override
+        public void onLocationChanged(Location location) {
+            PlayaPoint updatedHere = new PlayaPoint(location.getLatitude(), location.getLongitude(), location.getAccuracy(), location.getProvider());
+            playaPositioningSystem.setHere(updatedHere);
+            //Toast.makeText(MainActivity.this, "GPS Update",
+            //Toast.LENGTH_SHORT).show();
+            //PlayaDisplayBasics();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            //Toast.makeText(MainActivity.this, provider + "'s status changed to " + status,
+            //Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            //Toast.makeText(MainActivity.this, "Provider " + provider + " enabled",
+            //Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            //Toast.makeText(MainActivity.this, "Provider " + provider + " disabled!",
+            //Toast.LENGTH_SHORT).show();
+        }
     }
 }
